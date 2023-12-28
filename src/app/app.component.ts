ca7bc6d9ee736059f4830347976798242e0ac536
Verda Muso <gearmobile@gmail.com>
2023-12-28 20:57:25 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LargeComponentComponent } from '@components/large-component/large-component.component';
+import { InitialContentComponent } from '@components/initial-content/initial-content.component';
+import { LoadingSpinnerComponent } from '@components/loading-spinner/loading-spinner.component';
+import { ErrorMessageComponent } from '@components/error-message/error-message.component';
 
 @Component({
   selector: 'df-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LargeComponentComponent,
+    InitialContentComponent,
+    LoadingSpinnerComponent,
+    ErrorMessageComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'defer-angular';
+  public readonly title: string = 'Defer Angular';
 }
---
+++ src/app/components/large-component/large-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'df-large-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './large-component.component.html',
+  styleUrl: './large-component.component.scss'
+})
+export class LargeComponentComponent {
+
+}
